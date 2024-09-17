343c0cdf0b2af91c3d2d5f405c137fe151937bc3
Kifayath-paybolt <kifayatullah@paybolt.in>
2024-09-18 00:06:38 +0530

    Todo-app initialize

--- To-do/appnext/app/page.tsx
+++ To-do/appnext/app/page.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-center text-red-500">TO DO</h1>
+      const [tasks, setTasks] = useState<Task[]>([]);
+      const [newTask, setNewTask] = useState<string>('');
+      
+      const addTask = () =>{
+        if (newTask.trim() !== ''){
+          setTasks([...tasks, {text  : newTask, completed: false}]);        
+          setNewTask('');
+        }
+      };
     </main>
   );
 }
