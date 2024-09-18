31bbd599b08f9c3a634dad65b086586bfa4115f8
Kifayath-paybolt <kifayatullah@paybolt.in>
2024-09-18 11:34:17 +0530

    completed logic

--- To-do/appnext/app/page.tsx
+++ To-do/appnext/app/page.tsx
@@ -9,14 +9,27 @@ interface Task{
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
-      const [newTask, setNewTask] = useState<string>('');
-      
-      const addTask = () =>{
-        if (newTask.trim() !== ''){
-          setTasks([...tasks, {text  : newTask, completed: false}]);        
-          setNewTask('');
-        }
-      };
+  const [newTask, setNewTask] = useState<string>('');
+
+  const addTask = () =>{
+    if (newTask.trim() !== ''){
+      setTasks([...tasks, {text  : newTask, completed: false}]);
+      setNewTask('');
+    }
+  };
+
+  const updateComplete = (index: number)=>{
+    const updatedTasks = tasks.map((task, i) =>
+      i == index ? {...task, completed: !task.completed} : task
+  );
+  setTasks(updatedTasks);
+  };
+
+  const removeTask = (index : number) =>{
+    const updatedTasks = tasks.filter((_, i)=> i != index);
+    setTasks(updatedTasks);
+  }
+
   return (
     <main>
       <h1 className="text-center text-red-500">TO DO</h1>
